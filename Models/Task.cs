dc948341b2b9de28333498706114e1507e265191
hzhu3-hzhu3 <hzhu3@academic.rrc.ca>
2025-07-28 11:26:52 -0500

    Declare Task Class

---
+++ Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskHub.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Title must be at least 3 characters long.")]
+        public string? Title { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DueDate { get; set; }
+
+    }
+}
